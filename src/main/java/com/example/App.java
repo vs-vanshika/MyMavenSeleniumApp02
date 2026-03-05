ebfe56bc9d2b8cb2db8753daca730733e17dba23
vs-vanshika <vanshikashivamallu636@gmail.com>
2026-03-05 12:55:13 +0530

    first selenium commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+    }
+}
